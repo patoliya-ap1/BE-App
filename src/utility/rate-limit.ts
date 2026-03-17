36d7b542580846bbdb61310ec4630a4b51c643f9
patoliya-ap1 <patoliya.ap1@gmail.com>
2026-03-17 11:01:00 +0530

    ref v1

--- src/utility/rate-limit.ts
+++ src/utility/rate-limit.ts
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 100,
-  max: 10,
+  max: 100,
   message: { success: false, message: "Too many request, please try later" },
 });
